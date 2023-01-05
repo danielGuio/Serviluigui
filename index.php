2074fd0276cc8f29b80e79e42ead22234e75565d
danielGuio <91146714+danielGuio@users.noreply.github.com>
2023-01-05 18:02:05 -0500

    titulo del index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Soluciones para su hogar</title>
+        <title>Soluciones para el hogar</title>
         <link rel="stylesheet" href="Style.css"/>
         <script src="https://kit.fontawesome.com/c74707d321.js" crossorigin="anonymous"></script>
     </head>
